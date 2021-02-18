f78a2a1b742cef63d97580cb8ee31dab7789b4eb
ChrisStifter2021 <814623@mystma.org>
2021-02-18 16:01:13 +0000

    calendar

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+def calendar(m):
+    for i in range (0, len(months)):
+        if m == months[i]:
+            return i + 1
+
+        return p + "is not a month"
+
+        months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"] 
+
